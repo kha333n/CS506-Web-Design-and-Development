4e1f03b7da8b4f65ad67e8e3820bb8a1ebf8cefb
kha333n <44539272+kha333n@users.noreply.github.com>
2020-12-05 18:04:10 +0500

    Lecture 14

---
+++ src/com/kha333n/cs506/Lec 14/1. Retrieving Data from ResultSet/JdbcEx.java
@@ -0,0 +1,38 @@
+// File JdbcEx.java
+//step 1: import package
+import java.sql.*;
+
+public class JdbcEx {
+
+    //Command to link class and run
+    //java -cp .;mssql-jdbc.jar JdbcEx
+
+    public static void main(String args[]) {
+        try {
+//Step 2: load driver
+            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+//Step 3: define the connection URL
+            String url = "jdbc:sqlserver://KHA333N\\SQLEXPRESS";
+//Step 4: establish the connection
+            Connection con = DriverManager.getConnection(url,"jdbc","jdbc");
+//Step 5: create Statement
+            Statement st = con.createStatement();
+//Step 6: preapare & execute the query
+            String sql = "SELECT * FROM Person";
+            ResultSet rs = st.executeQuery(sql);
+//Step 7: process the results
+            while (rs.next()) {
+// The row name is "name" in database "PersonInfo,
+// hence specified in the getString() method.
+                String name = rs.getString("name");
+                String add = rs.getString("address");
+                String pNum = rs.getString("phoneNum");
+                System.out.println(name + " " + add + " " + pNum);
+            }
+//Step 8: close the connection
+            con.close();
+        } catch (Exception sqlEx) {
+            System.out.println(sqlEx);
+        }
+    } // end main
+} // end class
